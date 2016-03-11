849cb9e79b3bfe74a4c54c21f4ccf60a82abb01d
Pablo Baeyens <pbaeyens31@gmail.com>
2016-03-11 17:40:25 +0100

    .exe

--- P1/script.sh
+++ P1/script.sh
@@ -3,8 +3,8 @@
 # Ejemplo: fibonacci 10 5 80 ejecuta fibonacci 10, fibonacci 15, ..., fibonacci 80
 # Salida: nombredelejecutable_output.txt
 
-> $(basename $1 .out).dat
+> $(basename $1 .exe).dat
 
 for i in $(seq $2 $3 $4); do
-  ./$1 $i >> $(basename $1 .out).dat
+  ./$1 $i >> $(basename $1 .exe).dat
 done
