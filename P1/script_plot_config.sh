9b28196f80e581b30fedb5f43e6b8a87afdbf839
josemam <jmlosvillares@correo.ugr.es>
2016-03-12 19:19:50 +0100

    Script de config de plot para Windows
    Además he hecho cambios que permiten ahorrarnos el script que llama a
    gnuplot. Por otro lado he aislado el "set terminal _" a un archivo
    global para que podamos cambiar el formato según deseemos con mayor
    facilidad.

--- P1/script_plot_config.sh
+++ P1/script_plot_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Uso: nombredelejecutable nombre_eje_x nombre_eje_y nombre_titulo nombre_leyenda
-# Ejemplo: burbuja "Capacidad del vector" "Tiempos de ejecución" "Tiempos de ejecución de burbuja" "burbuja"
+# Ejemplo: ./script_plot_config.sh burbuja "Capacidad del vector" "Tiempos de ejecución" "Tiempos de ejecución de burbuja" "burbuja"
 # Salida: plot_nombredelejecutable.plg
 
 NAME=${1%.*}
@@ -8,10 +8,7 @@ LABELX=$2
 LABELY=$3
 TITLE=$4
 LEGEND=${5%.*}
-echo 'set terminal png
-set output out
-set title '\"$TITLE\"'
-set style data linespoints
+echo 'set title '\"$TITLE\"'
 set xlabel '\"$LABELX\"'
 set ylabel '\"$LABELY\"'
 plot data title '\"$LEGEND\"'' > plot_$NAME.plg
