bdb376116e24d87b229fe29e62e757d36223f90c
Pablo Baeyens <pbaeyens31@gmail.com>
2016-03-13 12:18:46 +0100

    Gnuplot sin scripts adicionales

--- P1/script_gnuplot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Uso: datos salida
-# Ejemplo: burbuja.dat burbuja.png
-# Salida: salida
-# Ejemplo de salida: burbuja.png
-
-PLOT_NAME=${2%.*}
-gnuplot -e "data='$1'" -e "out='$2'" plot_$PLOT_NAME.plg
-rm plot_$PLOT_NAME.plg
