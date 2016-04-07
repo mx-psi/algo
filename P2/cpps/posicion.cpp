771e540693b8ce945541d1b94b2b1d127fd9e140
Pablo Baeyens <pbaeyens31@gmail.com>
2016-04-07 13:19:28 +0200

    Cambia nombres para que coincidan con los de Antonio

--- P2/cpps/posicion.cpp
+++ P2/cpps/posicion.cpp
@@ -10,56 +10,42 @@
  using namespace std;
 
 
- /**
-    @brief Localiza un elemento en un vector ordenado
-    @pre `num_elem` >= 0
-    @param T: vector de elementos.
-    @param num_elem: número de elementos.
-    @return Posición del elemento si está, `-1` en otro caso
-    @note Versión obvia (búsqueda secuencial)
- */
- int posicion_trivial(const int T[], int num_elem){
-  for (int i = 0; i < num_elem; i++)
-   if(T[i] == i)
-    return i;
-  return -1;
- }
+/**
+   @brief Localiza un elemento en un vector ordenado
+   @pre `n` >= 0
+   @param v: vector de elementos.
+   @param v: número de elementos.
+   @return Posición del elemento si está, `-1` en otro caso
+   @note Versión obvia (búsqueda secuencial)
+*/
+int posicion_obvia(const int v[], int n){
+ for (int i = 0; i < n; i++)
+  if(v[i] == i)
+   return i;
+ return -1;
+}
 
-
- /**
-    @brief Localiza un elemento en un vector ordenado
-    @param T: vector de elementos.
-    @param ini: Posición en la que se comienza a buscar
-    @param fin: Posición en la que se termina
-    @return Posición del elemento si está, `-1` en otro caso
-    @note Función recursiva llamada por el *wrapper*
-    @see posicion_dyv1
- */
- int posicion_dyv1_rec(const int T[], int ini, int fin){
- }
+/**
+  @brief Localiza un elemento en un vector ordenado
+  @pre `n` >= 0
+  @param v: vector de elementos.
+  @param n: número de elementos.
+  @return Posición del elemento si está, `-1` en otro caso
+  @note Versión divide y vencerás 1 (recursiva)
+*/
+int posicion_dyv1(const int v[], int n){
+}
 
  /**
-    @brief Localiza un elemento en un vector ordenado
-    @pre `num_elem` >= 0
-    @param T: vector de elementos.
-    @param num_elem: número de elementos.
-    @return Posición del elemento si está, `-1` en otro caso
-    @note Versión divide y vencerás 1 (búsqueda binaria recursiva)
- */
- int posicion_dyv1(const int T[], int num_elem){
-  return posicion_dyv1_rec(T, 0, num_elem - 1);
- }
-
- /**
-    @brief Localiza un elemento en un vector ordenado
-    @pre `num_elem` >= 0
-    @param T: vector de elementos.
-    @param num_elem: número de elementos.
-    @return Posición del elemento si está, `-1` en otro caso
-    @note Versión divide y vencerás 2 (búsqueda binaria no recursiva)
- */
- int posicion_dyv2(const int T[], int num_elem){
- }
+  @brief Localiza un elemento en un vector ordenado
+  @pre `n` >= 0
+  @param v: vector de elementos.
+  @param n: número de elementos.
+  @return Posición del elemento si está, `-1` en otro caso
+  @note Versión divide y vencerás 2 (no recursiva)
+*/
+int posicion_dyv2(const int T[], int n){
+}
 
 
  int ejecutar(int (*f)(const int*, int), int n) {
@@ -68,7 +54,7 @@
 
     chrono::steady_clock::time_point tantes, tdespues;
    chrono::duration<double> transcurrido;
-   
+
    tantes = chrono::steady_clock::now();
    int i = f(T,n);
    tdespues = chrono::steady_clock::now();
@@ -83,18 +69,17 @@
  int main(int argc, char * argv[])
  {
 
-     if (argc != 3 || (argv[2][0] != 't' && argv[2][0] != '1' && argv[2][0] != '2'))
-     {
-       cerr << "Formato " << argv[0] << " <num_elem> t/1/2" << endl;
-       return -1;
-     }
+ if (argc != 3 || (argv[2][0] != 't' && argv[2][0] != '1' && argv[2][0] != '2')){
+  cerr << "Formato " << argv[0] << " <num_elem> t/1/2" << endl;
+  return -1;
+ }
 
-   int n = atoi(argv[1]);
+ int n = atoi(argv[1]);
 
-   if (argv[2][0] == 't')
-      return ejecutar(posicion_trivial, n);
-   else if (argv[2][0] == '1')
-      return ejecutar(posicion_dyv1, n);
-   else
-      return ejecutar(posicion_dyv2, n);
+ if (argv[2][0] == 't')
+    return ejecutar(posicion_obvia, n);
+ else if (argv[2][0] == '1')
+    return ejecutar(posicion_dyv1, n);
+ else
+    return ejecutar(posicion_dyv2, n);
  }
