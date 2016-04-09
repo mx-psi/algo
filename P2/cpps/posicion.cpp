40f7a26e7163daec39c911dd0a7462da44aa9d1a
Antonio Checa <antonio.checa.molina@gmail.com>
2016-04-09 19:57:08 +0200

    Dats y comparativa t y 1 de posición, y el algoritmo recursivo
    Hay dos parejas de dats y una imagen por cada intervalo. He hecho dos
    para que se vea la diferencia bien entre uno y otro y para que se vea el
    punto de corte bien (no hace falta que se incluyan las dos en la
    memoria, pero por si acaso). También he realizado las imágenes con la
    "ejecución media" de 100.000 ejecuciones, implementada en posicion.cpp,
    con el algoritmo recursivo.
    Falta comentar algunas cosas, jugar con el umbral y el algoritmo no
    recursivo.

--- P2/cpps/posicion.cpp
+++ P2/cpps/posicion.cpp
@@ -6,7 +6,9 @@
  #include <ctime>
  #include <cstdlib>
  #include <chrono>
+ #include <unistd.h>
  #include "generadores.h"
+
  using namespace std;
 
 
@@ -33,7 +35,33 @@ int posicion_obvia(const int v[], int n){
   @return Posición del elemento si está, `-1` en otro caso
   @note Versión divide y vencerás 1 (recursiva)
 */
+
+int posicion_dyv_rec(const int v[], int n, int ajuste = 0){
+  if(n == 1){
+		if(v[0] == ajuste)
+			return 0;
+		else
+			return -1;
+	}
+	else{
+		int medio = n/2;
+		if(medio == v[medio]-ajuste)
+			return medio;
+		else if(medio < v[medio]-ajuste)
+			return posicion_dyv_rec(v, n/2, ajuste);
+		else if(n == 2)
+        return -1;
+    else{
+			int pos_rel_der = posicion_dyv_rec(v+medio+1, (n-1)/2, medio+1+ajuste);
+			if(pos_rel_der != -1)
+				return pos_rel_der+medio+1;
+			else
+				return -1;
+		}
+	}
+}
 int posicion_dyv1(const int v[], int n){
+  return posicion_dyv_rec(v,n);
 }
 
  /**
@@ -60,7 +88,30 @@ int posicion_dyv2(const int T[], int n){
    tdespues = chrono::steady_clock::now();
 
    transcurrido = chrono::duration_cast<chrono::duration<double>>(tdespues - tantes);
-   cout << n << " " << i << " " << transcurrido.count() << endl;
+   cout << n << " " << transcurrido.count() << endl;
+   delete [] T;
+
+   return 0;
+ }
+const int MAX = 100000;
+ int ejecutar_media(int (*f)(const int*, int), int n, int MAX = MAX) {
+   int* T = new int[n];
+   double total = 0;
+   for(int m = 0; m < MAX; m++){
+     get_elementoensuposicion(T, n);
+
+      chrono::steady_clock::time_point tantes, tdespues;
+     chrono::duration<double> transcurrido;
+
+     tantes = chrono::steady_clock::now();
+     int i = f(T,n);
+     tdespues = chrono::steady_clock::now();
+
+     transcurrido = chrono::duration_cast<chrono::duration<double>>(tdespues - tantes);
+     total += transcurrido.count();
+   }
+   if(n != 5)
+    cout << n << " " << total/MAX << endl;
    delete [] T;
 
    return 0;
@@ -77,9 +128,9 @@ int posicion_dyv2(const int T[], int n){
  int n = atoi(argv[1]);
 
  if (argv[2][0] == 't')
-    return ejecutar(posicion_obvia, n);
+    return ejecutar_media(posicion_obvia, n, MAX);
  else if (argv[2][0] == '1')
-    return ejecutar(posicion_dyv1, n);
+    return ejecutar_media(posicion_dyv1, n, MAX);
  else
     return ejecutar(posicion_dyv2, n);
  }
