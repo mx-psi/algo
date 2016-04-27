b9023bda3c4fd4696a61a9bd4c9c23b972fb94c7
josemam <jmlosvillares@correo.ugr.es>
2016-04-27 19:56:18 +0200

    Algoritmos de peso máximo
    El cuore del commit corresponde a @AntonioCheca.

--- P3/cpps/contenedores.cpp
+++ P3/cpps/contenedores.cpp
@@ -6,20 +6,30 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
-
 using namespace std;
+
+typedef int peso_t;
 
 struct Cont{
   int id;
-  int peso;
+  peso_t peso;
 
-  Cont(int i, int p){
+  Cont(int i, peso_t p){
     id = i;
     peso = p;
   }
 };
 
-bool compara(Cont p1, Cont p2){return p1.peso > p2.peso;}
+bool menor(Cont p1, Cont p2){ return p1.peso < p2.peso; }
+bool mayor(Cont p1, Cont p2){ return p1.peso > p2.peso; }
+
+vector<Cont> a_cont(vector<peso_t> v) {
+  vector<Cont> conts;
+  for(int i = 0; i < v.size(); i++)
+    conts.push_back(Cont(i,v[i]));
+
+  return conts;
+}
 
 /**
    @brief Toma el mayor número de contenedores
@@ -27,36 +37,97 @@ bool compara(Cont p1, Cont p2){return p1.peso > p2.peso;}
    @param K: Capacidad máxima
    @return Vector de contenedores elegidos
 */
-vector<int> max_num_conts(vector<int> p, int K){
- vector<Cont> conts;
- for(int i = 0; i < p.size(); i++)
-   conts.push_back(Cont(i,p[i]));
- sort(conts.begin(), conts.end(), compara);
+vector<int> max_num_conts(vector<peso_t> p, peso_t K){
+  vector<Cont> conts = a_cont(p);
 
- vector<int> elegidos;
- int carga = 0;
+  sort(conts.begin(), conts.end(), menor);
 
- for(int i = 0; i < conts.size() && carga < K; i++){
-   if(carga + conts[i].peso <= K){
-     elegidos.push_back(conts[i].id);
-     carga += conts[i].peso;
+  vector<int> elegidos;
+
+  for(int i = 0; i < conts.size() && conts[i].peso <= K; i++){
+    elegidos.push_back(conts[i].id);
+    K -= conts[i].peso;
+  }
+
+  return elegidos;
+}
+
+vector<int> max_peso_greedy(vector<peso_t> p, peso_t capacidad){
+  vector<Cont> conts = a_cont(p);
+
+  sort(conts.begin(), conts.end(), mayor);
+  vector<int> elegidos;
+  for(int i = 0; i < conts.size() && capacidad > 0; i++){
+    if(conts[i].peso <= capacidad){
+      elegidos.push_back(conts[i].id);
+      capacidad -= conts[i].peso;
     }
- }
+  }
+  return elegidos;
+}
 
- return elegidos;
+peso_t suma(vector<Cont> v){
+  peso_t suma = 0;
+  for(vector<Cont>::iterator it = v.begin(); it != v.end(); ++it)
+    suma += it->peso;
+  return suma;
+}
+
+vector<Cont> bruto_sub(vector<Cont> p, peso_t K){
+  if(p.empty() || K == 0)
+    return vector<Cont>();
+
+  peso_t max = 0;
+  vector<Cont> max_v;
+  for(vector<Cont>::iterator it = p.begin(); it != p.end() && max < K; ++it){
+    vector<Cont>::iterator next(it);
+    vector<Cont> orig_copy(++next, p.end());   // Copia todos los posteriores
+    vector<Cont> v_candidato;
+    if(it->peso > K)
+      v_candidato = bruto_sub(orig_copy, K);
+    else {
+      v_candidato = bruto_sub(orig_copy, K-it->peso);
+      v_candidato.push_back(*it);   // Si usamos listas, mejor usar push_front
+    }
+    peso_t suma1 = suma(v_candidato);
+    if(suma1 > max && suma1 <= K){
+      max = suma1;
+      max_v = v_candidato;
+    }
+  }
+  return max_v;
+}
+
+vector<int> max_peso_bruto(vector<peso_t> v, peso_t K){
+  vector<Cont> v1 = a_cont(v);
+  vector<Cont> v2 = bruto_sub(v1, K);
+  vector<int> v3;
+  for(int i = 0; i < v2.size(); i++)
+    v3.push_back(v2[i].id);
+  return v3;
+}
+
+void print(const vector<int> v, const vector<peso_t> p) {
+  for(int i = 0; i < v.size(); i++)
+    cout << v[i] << " (" << p[v[i]] << ") ";
+
+  cout << '\n';
 }
 
 int main(){
- vector<int> pesos;
- int capacidad;
- vector<int> resultado;
- int k;
- cin >> capacidad;
- for(int i = 0; i < 10; i++){
+  vector<peso_t> pesos;
+  peso_t capacidad;
+  vector<int> res1, res2, res3;
+  peso_t k;
+  cin >> capacidad;
+  for(int i = 0; i < 10; i++){
     cin >> k;
     pesos.push_back(k);
   }
-  for(int i = 0; i < (int) resultado.size(); i++)
-    cout << resultado[i] << " ";
-  cout << endl;
+  res1 = max_num_conts(pesos, capacidad);
+  res2 = max_peso_greedy(pesos, capacidad);
+  res3 = max_peso_bruto(pesos, capacidad);
+  print(res1, pesos);
+  print(res2, pesos);
+  print(res3, pesos);
 }
