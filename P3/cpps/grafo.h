c2eebe7a7bd647ce454e5b7cdda730ed36735756
josemam <jmlosvillares@correo.ugr.es>
2016-04-28 16:53:38 +0200

    Datos y cosas en el TSP

--- P3/cpps/grafo.h
+++ P3/cpps/grafo.h
@@ -8,14 +8,15 @@
 
 typedef double peso_t;
 
-peso_t dist(peso_t[2] p1, peso_t[2] p2) {
+peso_t dist(peso_t p1[2], peso_t p2[2]) {
    return sqrt((p2[0]-p1[0])*(p2[0]-p1[0]) + (p2[1]-p1[1])*(p2[1]-p1[1]));
 }
 
 class Grafo {
    unsigned int nodos;
-   peso_t** lados;
+   peso_t* lados;
 
+public:
    Grafo(int n) :nodos(n) {
       lados = new peso_t[n*n];
    }
@@ -24,26 +25,26 @@ class Grafo {
       delete [] lados;
    }
    
-   Grafo(const Grafo* otro) :nodos(otro.nodos) {
+   Grafo(const Grafo& otro) :nodos(otro.nodos) {
       lados = new peso_t[nodos*nodos];
-      for (int i = 0; i < n; i++)
-         for (int j = 0; j < n; j++)
+      for (int i = 0; i < nodos; i++)
+         for (int j = 0; j < nodos; j++)
             lados[i*nodos+j] = otro.lados[i*nodos+j];
    }
    
-   unsigned int numNodos() { return nodos; }
+   unsigned int numNodos() const { return nodos; }
    
    void setPeso(int n1, int n2, peso_t peso) {
       lados[n1*nodos + n2] = lados[n2*nodos + n1] = peso;
    }
    
-   peso_t peso(int n1, int n2) {
+   peso_t peso(int n1, int n2) const {
       return lados[n1*nodos+n2];
    }
    
-   void pesosDesdeCoordenadas(peso_t[][2] c) {
-      for (int i = 0; i < n-1; i++)
-         for (int j = i+1; j < n; j++)
+   void pesosDesdeCoordenadas(peso_t c[][2]) {
+      for (int i = 0; i < nodos-1; i++)
+         for (int j = i+1; j < nodos; j++)
             setPeso(i, j, dist(c[i],c[j]));
    }
 };
--- P3/cpps/tsp.cpp
+++ P3/cpps/tsp.cpp
@@ -3,22 +3,44 @@
 */
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include <ctime>
 #include <cstdlib>
 #include <chrono>
 #include "grafo.h"
 using namespace std;
 
-int ejecutar(int (*f)(const Grafo*), const Grafo*) {
+vector<int> tsp_1(const Grafo& g) {
+   return {0,1}; // TODO: algoritmo 1
+}
+
+vector<int> tsp_2(const Grafo& g) {
+   return {24,13,15}; // TODO: algoritmo 2
+}
+
+vector<int> tsp_3(const Grafo& g) {
+   return {19,20,21}; // TODO: algoritmo 3
+}
+
+peso_t longitud(const vector<int> ids, const Grafo& g) {
+   peso_t l = g.peso(ids.back(), ids.front());
+   for (int i = 1; i < ids.size(); i++)
+      l += g.peso(ids[i], ids[i-1]);
+   return l;
+}
+
+int ejecutar(vector<int> (*f)(const Grafo&), const Grafo& g) {
   chrono::steady_clock::time_point tantes, tdespues;
   chrono::duration<double> transcurrido;
 
+  vector<int> ciclo;
   tantes = chrono::steady_clock::now();
-  f(g);
+  ciclo = f(g);
   tdespues = chrono::steady_clock::now();
 
   transcurrido = chrono::duration_cast<chrono::duration<double>>(tdespues - tantes);
-  cout << n << " " << transcurrido.count() << endl;
+  cout << longitud(ciclo, g) << " " << transcurrido.count() << endl;
 
   return 0;
 }
@@ -32,10 +54,19 @@ int main(int argc, char * argv[])
   }
   srand(time(0));
   int n;
-  /* TODO: Leer tamaño desde archivo */
-  Grafo g = new Grafo(n);
+  
+  ifstream f(argv[1]);
+  string s;
+  if (!(f >> s))
+     return -1;
+  f >> n;
+  Grafo g(n);
   peso_t coordenadas[n][2];
-  /* TODO: Generar vector de coordenadas desde archivo */
+  int id, x, y;
+  while(f >> id >> x >> y) {
+     coordenadas[id-1][0] = x;
+     coordenadas[id-1][1] = y;
+  }
   g.pesosDesdeCoordenadas(coordenadas);
 
   if (argv[2][0] == '1')
