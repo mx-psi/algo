b5d7eb6486c8c6b64aebf6aee80d33d52e5fe5c5
DarioSierra <dariosierra95@gmail.com>
2016-05-04 11:22:43 +0200

    algoritmo 2 terminado

--- P3/cpps/colonia.h
+++ P3/cpps/colonia.h
@@ -36,10 +36,10 @@ class Colonia {
          int siguiente_pos;
          if (uniforme() < P) {
             vector<double> prob_acumulada(1, 0);
-            for (vector<int>::iterator it = disponibles.begin(); it != disponibles.end() && !isinf(prob_acumulada.back()); ++it)
+            for (vector<int>::iterator it = disponibles.begin(); it != disponibles.end() && !std::isinf(prob_acumulada.back()); ++it)
                prob_acumulada.push_back(prob_acumulada.back() + probabilidad(actual, *it));
 
-            if (isinf(prob_acumulada.back()))   // Ocurre si dos nodos están a distancia 0
+            if (std::isinf(prob_acumulada.back()))   // Ocurre si dos nodos estï¿½n a distancia 0
                siguiente_pos = prob_acumulada.size()-1;
             else {
                double aleatorio = uniforme()*prob_acumulada.back();
@@ -60,10 +60,10 @@ class Colonia {
          }
          actual = disponibles[siguiente_pos];
          swap(disponibles[siguiente_pos], disponibles.back());
-         disponibles.pop_back();  // Para eliminar el nodo siguiente_pos-ésimo en O(1)
+         disponibles.pop_back();  // Para eliminar el nodo siguiente_pos-ï¿½simo en O(1)
          trayecto.push_back(actual);
       }
-      
+
       return trayecto;
    }
 
@@ -75,17 +75,17 @@ public:
    }
 
    Colonia(const Colonia& otra) :distancias(otra.distancias), feromonas(otra.feromonas) { };
-   
+
    // Itera y devuelve el mejor camino encontrado
    vector<int> itera(int iteraciones) {
       list<vector<int> > trayectos;
       for (int i = 0; i < iteraciones; i++)
          trayectos.push_back(un_trayecto());
-      
+
       for (int i = 1; i < nodos(); i++)
          for (int j = 0; j < i; j++)
             feromonas.setPeso(i, j, feromonas.peso(i, j)*EVAPORACION);
-      
+
       double mayor_suma = -1;
       list<vector<int> >::const_iterator mejor_camino, t;
       for (t = trayectos.cbegin(); t != trayectos.cend(); ++t) {
@@ -94,13 +94,13 @@ public:
             mayor_suma = suma;
             mejor_camino = t;
          }
-         
+
          for (int i = 1; i < (*t).size(); i++)
             feromonas.setPeso((*t)[i-1], (*t)[i], feromonas.peso((*t)[i-1], (*t)[i])+suma);
-            
+
          feromonas.setPeso((*t).front(), (*t).back(), feromonas.peso((*t).front(), (*t).back())+suma);
       }
-      
+
       return *mejor_camino;
    }
 };
--- P3/cpps/tsp.cpp
+++ P3/cpps/tsp.cpp
@@ -69,13 +69,15 @@ pair<int,vector<int>::iterator> PesoNuevoCircuito(list<int>::iterator nodo,vecto
 
   int PesoMin = g.peso(trayecto.back(),*nodo);
   PesoMin += g.peso(*nodo,trayecto.front());
+  PesoMin-= g.peso(trayecto.back(),trayecto.front());
 
   int IndiceInsercion = 0;
 
   for(int i = 1; i <= trayecto.size(); i++)
   {
-    int Peso = g.peso(trayecto[(i-1)%trayecto.size()],*nodo);
-    Peso += g.peso(*nodo,trayecto[i%trayecto.size()]);
+     int Peso = g.peso(trayecto[(i-1)%trayecto.size()],*nodo);
+     Peso += g.peso(*nodo,trayecto[i%trayecto.size()]);
+     Peso -=g.peso(trayecto[(i-1)%trayecto.size()],trayecto[i%trayecto.size()]);
 
     if (Peso < PesoMin){
       PesoMin = Peso;
@@ -87,14 +89,11 @@ pair<int,vector<int>::iterator> PesoNuevoCircuito(list<int>::iterator nodo,vecto
     return PesoIndice;
 }
 
+
 vector<int> tsp_2(const Grafo<peso_t>& g, const double coordenadas[][2]){
 
-  // // int longitudTotalCircuito = 0;
-  vector<int> trayecto = triangulo_inicial(coordenadas, g.numNodos());  // Inicializamos el vector con las ciudades que forman el mayor triángulo en el grafo.
 
-  // for(int i = 1; i <= trayecto.size(); i++)
-  //   longitudTotalCircuito += g.peso(trayecto[(i-1)%trayecto.size()],trayecto[i%trayecto.size()]); // Con esto sumamos los pesos de todos los nodos del ciclo.
-  //   //Lo del módulo es una mamonada porque me habeis pegado eso de poner menos lineas. Es para sumar la distancia entre el primero y el último y cerrar el ciclo.
+  vector<int> trayecto = triangulo_inicial(coordenadas, g.numNodos());  // Inicializamos el vector con las ciudades que forman el mayor triángulo en el grafo.
 
   list<int> disponibles;
   for (int i = 0; i < g.numNodos(); i++){
@@ -114,9 +113,6 @@ vector<int> tsp_2(const Grafo<peso_t>& g, const double coordenadas[][2]){
         }
       }
 
-      // cout << "PesoIndiceMin.second "<<*PesoIndiceMin.second<<endl<< "\t size\t"<<trayecto.size()<<"\n\n";
-      // cout << "llega hasa aqui \n\n insert minimo\n" << (PesoIndiceMin.second == trayecto.end() ? "sí" : "no");
-
       if (PesoIndiceMin.second == trayecto.end())
         trayecto.push_back(*Minimo);
       else
@@ -124,9 +120,6 @@ vector<int> tsp_2(const Grafo<peso_t>& g, const double coordenadas[][2]){
 
 
       disponibles.erase(Minimo);
-      //  cout<< "llega hasta aqui \n\n erase MINImo\n"<<endl;
-      // cout << "Minimo"<<" "<<*Minimo<<endl;
-
     }
 
   return trayecto; // TODO: algoritmo 2
@@ -139,21 +132,16 @@ vector<int> tsp_3(const Grafo<peso_t>& g, const double coordenadas[][2] = 0) {
    return c.itera(512);
 }
 
-void print(const vector<int> ids, ostream& os = cout) {
-  for (vector<int>::const_iterator i = ids.cbegin(); i != ids.cend(); ++i)
-    os << ((*i)+1) << '\n';
+void print(const vector<int> ids, bool a_archivo, ostream& os = cout) {
+  for (vector<int>::const_iterator i = ids.begin(); i != ids.end(); ++i)
+    os << (a_archivo ? (*i)+1 : *i) << (a_archivo ? '\n' : ' ');
 
   os << '\n';
 }
 
 string nombre_optimo(const char* nombre) {
-  string nm(nombre);
-  return nm.substr(0, nm.find_last_of(".")) + ".opt.tour";
-}
+  return string(nombre).substr(0, string(nombre).find_last_of(".")) + ".opt.tour";
 
-string nombre_salida(const char* nombre, char num) {
-  string nm(nombre);
-  return "resultados" + nm.substr(nm.find("TSP"), nm.find(".tsp")-nm.find("TSP")) + "_" + num + ".tour";
 }
 
 peso_t longitud_desde_archivo(string nombre, const Grafo<peso_t>& g) {
@@ -171,7 +159,7 @@ peso_t longitud_desde_archivo(string nombre, const Grafo<peso_t>& g) {
   return l + g.peso(primero-1,actual-1);
 }
 
-int ejecutar(vector<int> (*f)(const Grafo<peso_t>&, const double[][2]), const Grafo<peso_t>& g, const double coordenadas[][2], ostream& fo) {
+int ejecutar(vector<int> (*f)(const Grafo<peso_t>&, const double[][2]), const Grafo<peso_t>& g, const double coordenadas[][2], bool a_archivo) {
   chrono::steady_clock::time_point tantes, tdespues;
   chrono::duration<double> transcurrido;
 
@@ -180,52 +168,49 @@ int ejecutar(vector<int> (*f)(const Grafo<peso_t>&, const double[][2]), const Gr
   ciclo = f(g, coordenadas);
   tdespues = chrono::steady_clock::now();
 
-  print(ciclo, fo);
+  print(ciclo, a_archivo);
   transcurrido = chrono::duration_cast<chrono::duration<double>>(tdespues - tantes);
-  cout << longitud(ciclo, g) << " " << transcurrido.count() << endl;
+  if (!a_archivo)
+    cout << longitud(ciclo, g) << " " << transcurrido.count() << endl;
 
   return 0;
 }
 
 int main(int argc, char * argv[])
 {
-  if ((argc != 3 && argc != 4) || (argv[2][0] != '1' && argv[2][0] != '2' && argv[2][0] != '3' && argv[2][0] != 'o') || (argc == 4 && argv[3][0] != 't'))
+  if ((argc != 3 && argc != 4) || (argv[2][0] != '1' && argv[2][0] != '2' && argv[2][0] != '3') || (argc == 4 && argv[3][0] != 't'))
   {
-    cerr << "Formato " << argv[0] << " [datos].tsp 1/2/3/o [t]" << endl;
+    cerr << "Formato " << argv[0] << " [datos].tsp 1/2/3 [t]" << endl;
     return -1;
   }
   bool a_archivo = argc == 4;
   srand(time(0));
   int n;
 
-  ifstream fin(argv[1]);
+  ifstream f(argv[1]);
   string s;
-  if (!(fin >> s))
+  if (!(f >> s))
      return -1;
-  fin >> n;
+  f >> n;
   Grafo<peso_t> g(n);
   double coordenadas[n][2];
   int id;
   double x, y;
-  while(fin >> id >> x >> y) {
+  while(f >> id >> x >> y) {
      coordenadas[id-1][0] = x;
      coordenadas[id-1][1] = y;
   }
   g.pesosDesdeCoordenadas(coordenadas);
 
-  ofstream fout;
-  if (argv[2][0] != 'o') {
-    fout.open(nombre_salida(argv[1], argv[2][0]));
-    fout << "DIMENSION: " << g.numNodos() << '\n';
+  if (a_archivo)
+    cout << "DIMENSION: " << g.numNodos() << '\n';
+  else
+    cout << "Longitud óptima: " << longitud_desde_archivo(nombre_optimo(argv[1]), g) << '\n';
 
   if (argv[2][0] == '1')
-    return ejecutar(tsp_1, g, coordenadas, fout);
-  if (argv[2][0] == '2')
-    return ejecutar(tsp_2, g, coordenadas, fout);
-  if (argv[2][0] == '3')
-    return ejecutar(tsp_3, g, coordenadas, fout);
-  }
-
-  cout << "Longitud óptima: " << longitud_desde_archivo(nombre_optimo(argv[1]), g) << endl;
-  return 0;
+     return ejecutar(tsp_1, g, coordenadas, a_archivo);
+  else if (argv[2][0] == '2')
+     return ejecutar(tsp_2, g, coordenadas, a_archivo);
+  else
+     return ejecutar(tsp_3, g, coordenadas, a_archivo);
 }
