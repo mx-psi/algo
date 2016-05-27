9854fe4c6909bd63745d802a5d5b8244feb6031e
Pablo Baeyens <pbaeyens31@gmail.com>
2016-05-27 13:45:10 +0200

    Empieza tsp con cota

--- P4/cpps/tsp.cpp
+++ P4/cpps/tsp.cpp
@@ -16,36 +16,54 @@ using namespace std;
 
 //Luego se busca el mínimo recorriendo los relacionados de k-i para todo i en relacionados
 vector<int> RelacionadosCon(int k, int max, vector<int> camino){
-	vector<int> relacionados;
-	for(int i = 1; i <= max; i++)
-		if(find(camino.begin(), camino.end(), i) == camino.end() && i != k)
-			relacionados.push_back(i);
-	if(k != camino[camino.size()-1])
-		relacionados.push_back(1);
-	return relacionados;
+  vector<int> relacionados;
+  for(int i = 1; i <= max; i++)
+    if(find(camino.begin(), camino.end(), i) == camino.end() && i != k)
+      relacionados.push_back(i);
+  if(k != camino[camino.size()-1])
+    relacionados.push_back(1);
+  return relacionados;
 }
 
-/*
-int Cotasion1(vector<int>& camino, Grafo<int>& g){
+// Cota dada en el enunciado
+int cota1(vector<int>& camino, Grafo<int>& g){
 	int recorrido = 0;
+  // Peso del recorrido ya hecho
 	for(int i = 0; i < camino.size(); i++)
 		recorrido += g.peso(camino[i], camino[i+1]);
 
+  // Para cada i halla el peso mínimo entre los relacionados
+  for(int i = 0; i < g.numNodos(); i++){
+    if(find(camino.begin(), camino.end(), i) == camino.end()){
+      vector<int> v = RelacionadosCon(i,g.numNodos(),camino);
+      int min = 0;
+      for(int j = 1; j < v.size(); j++)
+        if(g.peso(i,j) < g.peso(i,min))
+          min = j;
+      recorrido += g.peso(i,min);
+    }
+  }
+  return recorrido;
 }
 
-int Ramificacion(vector<int>& camino, Grafo<int>& g){
-}
-*/
-
-vector<int> tsp_4(const Grafo<peso_t>& g, const double coordenadas[][2] = 0) {
-  // TODO: Algoritmo branch and bound con función de acotación descrita
+// Nuestra función de acotación
+int cota2(vector<int>& camino, Grafo<int>& g){
+  return 0; // TODO
 }
 
-vector<int> tsp_5(const Grafo<peso_t>& g, const double coordenadas[][2] = 0) {
-  // TODO: Algoritmo branch and bound con alguna otra función de acotación
+vector<int> tsp_cota(const Grafo<peso_t>& g, int (*cota)(vector<int>&,Grafo<int>&)) {
+  priority_queue<vector<int>> cola;
 }
 
-vector<int> tsp_6(const Grafo<peso_t>& g, const double coordenadas[][2] = 0) {
+vector<int> tsp_1(const Grafo<peso_t>& g) {
+  return tsp_cota(g,cota1);
+}
+
+vector<int> tsp_2(const Grafo<peso_t>& g) {
+  return tsp_cota(g,cota2);
+}
+
+vector<int> tsp_3(const Grafo<peso_t>& g) {
   // TODO: Backtracking con función de acotación descrita
 }
 
@@ -82,13 +100,13 @@ peso_t longitud_desde_archivo(string nombre, const Grafo<peso_t>& g) {
   return l + g.peso(primero-1,actual-1);
 }
 
-int ejecutar(vector<int> (*f)(const Grafo<peso_t>&, const double[][2]), const Grafo<peso_t>& g, const double coordenadas[][2], ostream& fo) {
+int ejecutar(vector<int> (*f)(const Grafo<peso_t>&), const Grafo<peso_t>& g, ostream& fo) {
   chrono::steady_clock::time_point tantes, tdespues;
   chrono::duration<double> transcurrido;
 
   vector<int> ciclo;
   tantes = chrono::steady_clock::now();
-  ciclo = f(g, coordenadas);
+  ciclo = f(g);
   tdespues = chrono::steady_clock::now();
 
   print(ciclo, fo);
@@ -98,13 +116,13 @@ int ejecutar(vector<int> (*f)(const Grafo<peso_t>&, const double[][2]), const Gr
   return 0;
 }
 
-vector<int> (*const tsp[])(const Grafo<peso_t>&, const double[][2]) = {tsp_4, tsp_4, tsp_4, tsp_4, tsp_5, tsp_6};
+vector<int> (*const tsp[])(const Grafo<peso_t>&) = {tsp_1, tsp_2, tsp_3};
 
 int main(int argc, char * argv[])
 {
-  if ((argc != 3 && argc != 4) || ((argv[2][0] < '4' || argv[2][0] > '6') && argv[2][0] != 'o') || (argc == 4 && argv[3][0] != 't'))
+  if ((argc != 3 && argc != 4) || ((argv[2][0] < '1' || argv[2][0] > '3') && argv[2][0] != 'o') || (argc == 4 && argv[3][0] != 't'))
   {
-    cerr << "Formato " << argv[0] << " [datos].tsp 1/2/3/4/5/6/o [t]" << endl;
+    cerr << "Formato " << argv[0] << " [datos].tsp 1/2/3/o [t]" << endl;
     return -1;
   }
   srand(time(0));
@@ -129,7 +147,7 @@ int main(int argc, char * argv[])
   if (argv[2][0] != 'o') {
     fout.open(nombre_salida(argv[1], argv[2][0]));
     fout << "DIMENSION: " << g.numNodos() << '\n';
-    return ejecutar(tsp[argv[2][0] - '1'], g, coordenadas, fout);
+    return ejecutar(tsp[argv[2][0] - '1'], g, fout);
   }
 
   cout << "Longitud óptima: " << longitud_desde_archivo(nombre_optimo(argv[1]), g) << endl;
